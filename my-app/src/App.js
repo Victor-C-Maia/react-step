336323034da1753cb64e7a419718622b6e79d90b
= <=>
2024-11-09 17:03:00 -0300

    alterações aula02

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,23 @@
 // App.js
 import React from "react";
-import StyledButton from "./components/Aula 9/StyledButton";
-import StyledCard from "./components/Aula 9/StyledCard";
-import ExampleMockApi from "./components/Aula 9/ExampleMockApi";
+import StyledButton from "./components/Aula-9/StyledButton";
+import StyledCard from "./components/Aula-9/StyledCard";
+import ExampleMockApi from "./components/Aula-9/ExampleMockApi";
+import HelloWorld from "./HelloWorld";
+import HelloWorld2 from "./HelloWorld2";
+import Pessoa from "./Pessoa";
+import Contador from "./Contador";
+import MudaCorFundo from "./MudaCorFundo";
 
 const App = () => {
   return (
     <>
-      <ExampleMockApi />
+      {/* <HelloWorld /> */}
+      {/* <HelloWorld2 /> */}
+      {/* <Pessoa nome="Zé" sobrenome="das Couves"/> */}
+      {/* <ExampleMockApi /> */}
+      <Contador />
+      {/* <MudaCorFundo /> */}
     </>
   );
 };
